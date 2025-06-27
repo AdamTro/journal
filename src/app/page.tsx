878ce1f9d3c07c387b72f90a9983653b7033707f
Trojette <ATROJET2@volvocars.com>
2025-06-27 13:48:03 +0200

    Lade till startsida

---
+++ src/app/page.tsx
@@ -0,0 +1,8 @@
+export default function HomePage() {
+  return (
+    <main style={{ padding: "2rem" }}>
+      <h1>Välkommen till journal.testum.se</h1>
+      <p>Nu fungerar det!</p>
+    </main>
+  );
+}
